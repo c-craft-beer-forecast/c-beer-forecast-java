3ca57b4bd61a5ce9374d0d48bdf99969c467f273
Nagisa <nagisa.sekiguchi@tcs.com>
2025-06-16 16:40:00 +0900

    first commit

---
+++ src/main/java/com/beer/beer_forecast/BeerForecastApplication.java
@@ -0,0 +1,13 @@
+package com.beer.beer_forecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerForecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerForecastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beer/beer_forecast/BeerForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.beer_forecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
